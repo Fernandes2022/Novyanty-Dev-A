0cbf11850d8255c76dfd657ae22538f265cca213
princeflexzy0 <emmanuelatere44@gmail.com>
2025-10-27 10:15:09 +0000

    fix: remove extra closing motion.div tag

--- app/page.tsx
+++ app/page.tsx
@@ -269,7 +269,6 @@ export default function Home() {
               </motion.div>
             </motion.div>
           </motion.div>
-        </motion.div>
       </section>
 
       <GradientDivider />
