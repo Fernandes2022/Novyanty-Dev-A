c648d4b47df487d6ef471e2e0bd1c49e686c4038
princeflexzy0 <emmanuelatere44@gmail.com>
2025-11-04 06:01:43 +0000

    fix: remove broken videoRef references

--- app/page.tsx
+++ app/page.tsx
@@ -130,15 +130,6 @@ export default function Home() {
   };
 
   useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.play().catch(err => {
-        console.log('Video autoplay prevented:', err);
-        document.addEventListener('touchstart', () => {
-          videoRef.current?.play();
-        }, { once: true });
-      });
-    }
-  }, []);
 
   const disableHeavy = typeof window !== "undefined" && shouldDisableHeavyEffects();
   return (
